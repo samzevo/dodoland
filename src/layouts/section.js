41bfd469e284faa4b87303639d5ac12f440bb392
samzevo <samzevo@gmail.com>
2021-01-30 16:59:21 +0530

    added backdrop

--- src/layouts/section.js
+++ src/layouts/section.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Layout from '../styles/sheets/layouts'
+
 const Section = (props) => {
-	return <div style={Layout.sectionDefault}>{props.children}</div>
+	return (
+		<div style={props.style ? props.style : Layout.sectionDefault}>
+			{props.children}
+		</div>
+	)
 }
 
 export default Section
--- src/styles/sheets/layouts/index.js
+++ src/styles/sheets/layouts/index.js
@@ -1,22 +1,23 @@
-
-const Layouts={
-    containerDefault:{
-        margin:'10px',
-        
-    },
-    sectionDefault:{
-        width:'100%',
-    },
-    row:{
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center'
-    },
-    column:{
-        display:'flex',
-        flexDirection:'column',
-    }
+const Layouts = {
+	containerDefault: {
+		margin: '10px',
+	},
+	sectionDefault: {
+		width: '100vw',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	row: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	column: {
+		display: 'flex',
+		flexDirection: 'column',
+	},
 }
 
-export default Layouts;
+export default Layouts
