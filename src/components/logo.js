e498e97a8d6a39cc36ecf1b91e3c58b894d392ce
samzevo <samzevo@gmail.com>
2021-01-30 12:18:14 +0530

    added screens folder

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { logo } from '../styles/sheets'
 
 function Logo(props) {
-	const logo = (
+	return (
 		<img
 			style={props.style ? props.style : logo.default}
 			src={props.image}
 			alt='logo'
 		/>
 	)
-	return logo
 }
 
 export default Logo
