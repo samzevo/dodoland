e73613ef0e8ec8b5f30c130a020900f35ada1b3a
samzevo <samzevo@gmail.com>
2021-01-30 13:19:52 +0530

    added patterns

--- src/styles/base/typography.js
+++ src/styles/base/typography.js
@@ -1,18 +1,16 @@
-
-const Typography={
-    textDefault:{
-        fontFamily:'Larsseit',
-        fontSize:'14px',
-        fontWeight:'normal',
-        letterSpacing:'0.1em',
-    },
-    titleDefault:{
-        fontFamily:'Larsseit',
-        fontSize:'18px',
-        fontWeight:500,
-        letterSpacing:'0.08em',
-    },
-
+const Typography = {
+	textDefault: {
+		fontFamily: 'Larsseit',
+		fontSize: '14px',
+		fontWeight: 'normal',
+		letterSpacing: '0.1em',
+	},
+	titleDefault: {
+		fontFamily: 'Larsseit',
+		fontSize: '18px',
+		fontWeight: 500,
+		letterSpacing: '0.08em',
+	},
 }
 
-export default Typography;
+export default Typography
