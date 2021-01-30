d7ec33812168a07da8814857fe20b02fad98d93c
samzevo <samzevo@gmail.com>
2021-01-30 19:11:08 +0530

    added header

--- src/patterns/navigation/footer.js
+++ src/patterns/navigation/footer.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import { Logo } from '../../components'
 import { Open_sealogo, Facebook, Instagram, Twitter } from '../../assets/assets'
-import { Row, Column,Conta } from '../../layouts/index'
+import { Section, Row, Container } from '../../layouts'
+import { section } from '../../styles/sheets/layouts'
 
 function Footer(props) {
 	const renderSocialMediaIcons = (
-		<div>
+		<Row>
 			<Logo image={Facebook} />
 			<Logo image={Instagram} />
 			<Logo image={Twitter} />
-		</div>
+		</Row>
 	)
 
 	return (
-		<Row>
-				<Logo image={Open_sealogo} />
-				{renderSocialMediaIcons}
-		</Row>
+		<Section
+			style={{
+				...section.default,
+				position: 'fixed',
+				bottom: 0,
+				left: 0,
+				justifyContent: 'space-around',
+				alignItems: 'center',
+				margin: 32,
+			}}>
+			<Logo image={Open_sealogo} />
+			{renderSocialMediaIcons}
+		</Section>
 	)
 }
 
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-    return (
-        <div>
-            Home hi
-        </div>
-    )
+	return <div></div>
 }
 
-export default Home;
+export default Home
