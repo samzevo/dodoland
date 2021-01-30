6ecf26042a4373309fed0fa443c0c1ddf37124ff
samzevo <samzevo@gmail.com>
2021-01-30 12:49:38 +0530

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import Logo from './components/logo'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Button from './components/button'
-import Navbar from './patterns/navigation/navbar'
-
-import { logo } from './styles/sheets'
 
 function App() {
 	const style = {
 		background: 'grey',
 	}
 	return (
-		<div style={}>
-			<Router>
-				<Navbar />
-			</Router>
-			<Logo style={logo.default} />
-			<Button text='Get Started' />
-			<Button text='Guide' />
+		<div>
+			<Router></Router>
 		</div>
 	)
 }
