daba4cab6c487b05126d457531a9cc56fcaeb0e6
Arun <arunram20001136@gmail.com>
2021-02-22 16:51:43 +0530

    'added'

--- src/patterns/navigations/header.js
+++ src/patterns/navigations/header.js
@@ -13,15 +13,20 @@ import Sidebar from './sidebar'
 import logo from '../../assets/brandings/logo/logo.svg'
 import wallet from '../../assets/icons/wallet.svg'
 import hamburger from '../../assets/icons/hamburger.svg'
+import close from '../../assets/icons/close.svg'
 
 const Header = () => {
 
     const [isOpen, setIsOpen] = useState(false)	
+    const [btnOpen,setBtnOpen] = useState(true)
 
     const toggle = () => {
 		setIsOpen(!isOpen)
 	}
 
+    const toggleBtn = () => {
+        setBtnOpen(!btnOpen)
+    }
 
     const renderLinks=(
         <div className="nav-links">
@@ -34,15 +39,30 @@ const Header = () => {
                <Link name="Upcoming" path="/upcoming" />
         </div>
     )
+
     return (
         <div className="header">
            <Logo src={logo} className="logo" />
            {renderLinks}
-           <Button variant="btnIcon"><Icon src={wallet} style={{marginRight:'10px'}} />connect wallet</Button>
+           <Button variant="btnIcon" onClick={toggleBtn}><Icon src={wallet} style={{marginRight:'10px'}}  />connect wallet</Button>
            <div className="hamburger" onClick={toggle}>
                 <Icon src={hamburger} />
            </div>
            { isOpen ? <Sidebar toggle={toggle} /> : null}
+           { btnOpen ? <div className="backdrop"></div> : null}
+           { btnOpen ? (
+               <div className="modal-header"
+                 style={{
+                     top:btnOpen ? '50%' : '-100%'
+                 }}
+               >
+                   <span onClick={toggleBtn}><Icon src={close} /></span>
+                   <div>
+                       <Button variant="secondary">meta mask</Button>
+                       <Button variant="secondary">wallet connect</Button>
+                   </div>
+               </div>
+           ) : null}
         </div>
     )
 }
